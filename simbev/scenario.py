013ba139c395943ceb5a73b17eedd55b4947ac9d
Moritz Schiel <moritz.schiel@gmx.net>
2021-10-25 15:05:02 +0200

    remove run function from scenario #41

--- simbev/scenario.py
+++ simbev/scenario.py
@@ -1,18 +1,14 @@
 import pandas as pd
 from simbev import car
 
+
 class Scenario:
-    def __init__(self, config):
-        self.regio = config
+    def __init__(self, regions: pd.DataFrame):
+        self.regions = regions
+        self.region_count = len(regions.index)
 
-    def run(self, timedelta, start, end):
-        time = pd.date_range(start, end, freq=timedelta)
-        for i in time:
-            car.status = 3
-            # driving
-            # parking
-            # charging
 
-config = 71
-s1 = Scenario(config)
-s1.run('H', '2017-1-1', '2018-1-1')
+if __name__ == '__main__':
+    region_list = pd.DataFrame()
+    s1 = Scenario(region_list)
+    s1.run('H', '2017-1-1', '2018-1-1')
