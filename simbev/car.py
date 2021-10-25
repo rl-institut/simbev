191b04737bd8aa68204580297d25e62dbeb0d1e2
Moritz Schiel <moritz.schiel@gmx.net>
2021-10-25 17:54:02 +0200

    add cars to region via function #41

--- simbev/car.py
+++ simbev/car.py
@@ -1,7 +1,8 @@
 class Car:
-    def __init__(self, battery_capacity: float, charging_curve, consumption: float,
+    def __init__(self, battery_capacity: float, charging_capacity, charging_curve, consumption: float,
                  soc: float, car_type: str, work_station, home_station, status: str = "parking"):
         self.battery_capacity = battery_capacity
+        self.charging_capacity = charging_capacity
         self.charging_curve = charging_curve
         self.consumption = consumption
         self.soc = soc
--- simbev/region.py
+++ simbev/region.py
@@ -1,5 +1,20 @@
+import pandas as pd
+from car import Car
+
+
 class Region:
-    def __init__(self, region_id, region_type, car_dict):
+    def __init__(self, region_id, region_type, car_list=[]):
         self.id = region_id
         self.type = region_type
-        self.cars = car_dict
+        self.cars = car_list
+
+    def add_cars_from_config(self, car_dict, tech_data: pd.DataFrame):
+        for car_type, car_count in car_dict.items():
+            for i in range(car_count):
+                bat_cap = tech_data.at[car_type, 'battery_capacity']
+                consumption = tech_data.at[car_type, 'energy_consumption']
+                charging_capacity_slow = tech_data.at[car_type, 'max_charging_capacity_slow']
+                charging_capacity_fast = tech_data.at[car_type, 'max_charging_capacity_fast']
+                charging_capacity = {'slow': charging_capacity_slow, 'fast': charging_capacity_fast}
+                new_car = Car(bat_cap, charging_capacity, [], consumption, 1, car_type, None, None)  # TODO: check all parameters
+                self.cars.append(new_car)
