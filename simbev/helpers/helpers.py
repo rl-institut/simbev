ea290a9c07b1dcbc366950235dde12fa56b1a351
Moritz Schiel <moritz.schiel@gmx.net>
2021-12-01 16:37:50 +0100

    add function to compile output csv #45

--- simbev/helpers/helpers.py
+++ simbev/helpers/helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import datetime as dt
 
 
 def single_to_multi_scenario(region_type,
@@ -54,14 +55,57 @@ def single_to_multi_scenario(region_type,
 
 def compile_output(dir: Path, start, end, timestep=15, file_name="output.csv"):  # maybe convert dir into Path
     # create Dataframe, take start and end date + timestep as parameter, build timeseries as index
+    dt_range = pd.date_range(start, end + dt.timedelta(days=1), freq=str(timestep)+'min')
+    pd_result = pd.DataFrame(0.0, index=range(len(dt_range)),
+                             columns=["time", "sum CS power", "sum UC work", "sum UC business", "sum UC school",
+                                      "sum UC shopping", "sum UC private/ridesharing", "sum UC leisure",
+                                      "sum UC home", "sum UC hub"])
     # fill rest with zeroes
+    pd_result["time"] = dt_range
     # columns:
     # sum CS power;sum UC work;sum UC business;sum UC school;sum UC shopping;
     # sum UC private/ridesharing;sum UC leisure;sum UC home;sum UC hub
+
     # run through all csv result files of this run that include "standing_times" in the title
     for file in dir.rglob("*.csv"):
-        file_df = pd.read_csv(file)
-        # go through all events, if charging then add power at that time step to the use case
-    # at the end combine all sums in column sum CS power
+        if "standing_times" in file.name:
+            file_df = pd.read_csv(file, sep=',', decimal='.')
+            # file_df relevant columns: location,netto_charging_capacity,chargingdemand,charge_time,park_start,park_end
+            for i in file_df.index:
+                demand = file_df.loc[i, "chargingdemand"]
+                if demand > 0:
+                    # extract parameters for the charging event
+                    uc = file_df.loc[i, "location"].split('_')
+                    col = "sum UC " + uc[-1]
+                    charge_time = file_df.loc[i, "charge_time"]
+                    park_start = file_df.loc[i, "park_start"]
+                    cap = file_df.loc[i, "netto_charging_capacity"]
+                    max_charge = cap * timestep / 60
+                    # average power in each time step
+                    power = []
+                    for k in range(charge_time):
+                        # if possible charge with max power, greedy strat
+                        if demand >= max_charge:
+                            power.append(cap)
+                            demand -= max_charge
+                        else:
+                            power.append(demand / timestep * 60)
+                            demand = 0
+                    # add charging series to result pandas
+                    for count, p in enumerate(power):
+                        if park_start + count < len(pd_result.index):
+                            pd_result.loc[park_start + count, col] += p
+                        else:
+                            print("There is " + str(p) + " kW to charge in timestep " + str(park_start + count))
 
-    return
+    pd_result["sum CS power"] = pd_result["sum UC work"] + pd_result["sum UC business"] + pd_result["sum UC school"] \
+                                + pd_result["sum UC shopping"] + pd_result["sum UC private/ridesharing"] \
+                                + pd_result["sum UC leisure"] + pd_result["sum UC hub"] + pd_result["sum UC home"]
+
+    pd_result.to_csv(Path(dir, file_name), sep=',', decimal='.')
+
+
+if __name__ == '__main__':
+    start = dt.datetime(2021, 9, 17)
+    end = dt.datetime(2021, 9, 30)
+    compile_output(Path('..', 'res', 'default_single_2021-11-22_133348_simbev_run'), start, end)
