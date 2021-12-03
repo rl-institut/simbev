d2783f83a570322af5368e43f47469c323f6a6c4
Moritz Schiel <moritz.schiel@gmx.net>
2021-12-03 15:51:49 +0100

    add output per region #45

--- simbev/helpers/helpers.py
+++ simbev/helpers/helpers.py
@@ -53,7 +53,27 @@ def single_to_multi_scenario(region_type,
     return regions, tech_data
 
 
-def compile_output(dir: Path, start, end, timestep=15, file_name="output.csv"):  # maybe convert dir into Path
+# TODO: config option
+def compile_output(result_dir: Path, start, end, region_mode, timestep=15):
+    """
+
+    Parameters
+    ----------
+    result_dir : :obj:`Path`
+        Path to scenario results
+    start : :obj:`datetime`
+        starting time of simulation
+    end : :obj:`datetime`
+        ending time of simulation
+    region_mode : :obj:`string`
+        single or multi region simulation
+    timestep : :obj:`int`
+        time step of simulation in minutes
+
+    Returns
+    -------
+
+    """
     # create Dataframe, take start and end date + timestep as parameter, build timeseries as index
     dt_range = pd.date_range(start, end + dt.timedelta(days=1), freq=str(timestep)+'min')
     pd_result = pd.DataFrame(0.0, index=range(len(dt_range)),
@@ -62,13 +82,18 @@ def compile_output(dir: Path, start, end, timestep=15, file_name="output.csv"): 
                                       "sum UC home", "sum UC hub"])
     # fill rest with zeroes
     pd_result["time"] = dt_range
+    pd_result_sum = pd_result.copy()
+    power_columns = ["sum CS power", "sum UC work", "sum UC business", "sum UC school",
+                     "sum UC shopping", "sum UC private/ridesharing", "sum UC leisure",
+                     "sum UC home", "sum UC hub"]
     # columns:
     # sum CS power;sum UC work;sum UC business;sum UC school;sum UC shopping;
     # sum UC private/ridesharing;sum UC leisure;sum UC home;sum UC hub
 
     # run through all csv result files of this run that include "standing_times" in the title
-    for file in dir.rglob("*.csv"):
-        if "standing_times" in file.name:
+    sub_dirs = [f for f in result_dir.iterdir() if f.is_dir()]
+    for sub_dir in sub_dirs:
+        for file in sub_dir.rglob("*standing_times.csv"):
             file_df = pd.read_csv(file, sep=',', decimal='.')
             # file_df relevant columns: location,netto_charging_capacity,chargingdemand,charge_time,park_start,park_end
             for i in file_df.index:
@@ -99,11 +124,17 @@ def compile_output(dir: Path, start, end, timestep=15, file_name="output.csv"): 
                             # print("There is " + str(p) + " kW to charge in timestep " + str(park_start + count))
                             break
 
-    pd_result["sum CS power"] = pd_result["sum UC work"] + pd_result["sum UC business"] + pd_result["sum UC school"] \
-                                + pd_result["sum UC shopping"] + pd_result["sum UC private/ridesharing"] \
-                                + pd_result["sum UC leisure"] + pd_result["sum UC hub"] + pd_result["sum UC home"]
+        pd_result["sum CS power"] = (pd_result["sum UC work"] + pd_result["sum UC business"] + pd_result["sum UC school"] +
+                                     pd_result["sum UC shopping"] + pd_result["sum UC private/ridesharing"] +
+                                     pd_result["sum UC leisure"] + pd_result["sum UC hub"] + pd_result["sum UC home"])
 
-    pd_result.to_csv(Path(dir, file_name), sep=',', decimal='.')
+        pd_result.to_csv(Path(result_dir, sub_dir.name + ".csv"), sep=',', decimal='.')
+        if region_mode == "multi":
+            pd_result_sum[power_columns] += pd_result[power_columns]
+        pd_result[power_columns] = 0.0
+
+    if region_mode == "multi":
+        pd_result_sum.to_csv(Path(result_dir, "0_results_all_regions.csv"), sep=',', decimal='.')
 
 
 if __name__ == '__main__':
