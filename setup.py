8f4648aee1bd110d7e2e9840b416d37bbfcb35b8
timroepcke <tim.roepcke@rl-institut.de>
2021-12-20 17:17:55 +0100

    update setup

--- setup.py
+++ setup.py
@@ -30,38 +30,29 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="rli_template",  # Required
+    name="simbev",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.0",  # Required
+    version="0.1.0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Description",  # Optional
-    # This is an optional longer description of your project that represents
-    # the body of text which users will see when they visit PyPI.
-    #
-    # Often, this is the same as your README, so you can just read it in from
-    # that file directly (as we have already done above)
-    #
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
-    # Denotes that our long_description is in Markdown; valid values are
-    # text/plain, text/x-rst, and text/markdown
-    #
-    # Optional if long_description is written in reStructuredText (rst) but
-    # required for plain-text or Markdown; if unspecified, "applications should
-    # attempt to render [the long_description] as text/x-rst; charset=UTF-8 and
-    # fall back to text/plain if it is not valid rst" (see link below)
-    #
-    # This field corresponds to the "Description-Content-Type" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type="text/markdown",  # Optional (see note above)
+    description="Simulation of electric vehicle charging demand",  # Optional
+    # # Denotes that our long_description is in Markdown; valid values are
+    # # text/plain, text/x-rst, and text/markdown
+    # #
+    # # Optional if long_description is written in reStructuredText (rst) but
+    # # required for plain-text or Markdown; if unspecified, "applications should
+    # # attempt to render [the long_description] as text/x-rst; charset=UTF-8 and
+    # # fall back to text/plain if it is not valid rst" (see link below)
+    # #
+    # # This field corresponds to the "Description-Content-Type" metadata field:
+    # # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
+    # long_description_content_type="text/markdown",  # Optional (see note above)
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
@@ -91,20 +82,16 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords="",  # Optional
+    # # This field adds keywords for your project which will appear on the
+    # # project page. What does your project relate to?
+    # #
+    # # Note that this is a string of words separated by whitespace, not a list.
+    # keywords="",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={"": "src"},  # Optional
+    package_dir={"": "simbev"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -114,29 +101,29 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where="src"),  # Required
+    packages=find_packages(where="simbev"),  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires=">=3, <4",
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={"dev": [], "test": []},  # Optional
+    python_requires=">=3.9",
+    # # This field lists other packages that your project depends on to run.
+    # # Any package you put here will be installed by pip when your project is
+    # # installed, so they must be valid existing projects.
+    # #
+    # # For an analysis of "install_requires" vs pip's requirements files see:
+    # # https://packaging.python.org/en/latest/requirements.html
+    # install_requires=[configparser, numpy, pandas],  # Optional
+    # # List additional groups of dependencies here (e.g. development
+    # # dependencies). Users will be able to install these using the "extras"
+    # # syntax, for example:
+    # #
+    # #   $ pip install sampleproject[dev]
+    # #
+    # # Similar to `install_requires` above, these must be valid existing
+    # # projects.
+    # extras_require={"dev": [], "test": []},  # Optional
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
@@ -173,7 +160,7 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        "Bug Reports": "https://github.com/rl-institut/rli_template/issues",
-        "Source": "https://github.com/rl-institut/rli_template",
+        "Bug Reports": "https://github.com/rl-institut/simbev/issues",
+        "Source": "https://github.com/rl-institut/simbev",
     },
 )
