4e82b84330471599d6462fb4f1ce3060e60103e7
mosc5 <moritz.schiel@rl-institut.de>
2022-05-05 17:10:04 +0200

    change scenario.py to simbev and add all class variables #41

---
+++ simbev/simbev.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+from simbev import car
+from region import Region
+
+
+class SimBEV:
+    def __init__(self, region_data: pd.DataFrame, charging_prob_dict,
+                 tech_data: pd.DataFrame, config_dict):
+        # parameters from arguments
+        self.region_data = region_data
+        self.charging_prob = charging_prob_dict
+        self.tech_data = tech_data
+
+        # parameters from config_dict
+        self.step_size = config_dict("step_size")
+        self.soc_min = config_dict("soc_min")
+        self.rng = config_dict("rng")
+        self.eta_cp = config_dict("eta_cp")
+        self.start_date = config_dict("start_date")
+        self.end_data = config_dict("end_date")
+
+        # additional parameters
+        self.regions = []
+
+    def add_regions_from_dataframe(self):
+        for i in range(len(self.region_data.index)):
+            region_id = self.region_data.iat[i, 0]
+            region_type = self.region_data.iat[i, 1]
+            car_dict = self.region_data.iloc[i, 2:].to_dict()
+            new_region = Region(region_id, region_type)
+            new_region.add_cars_from_config(car_dict, self.tech_data)
+            self.regions.append(new_region)
+
+    def _add_parameters_from_config(self):
+
+
+
+# tests
+if __name__ == '__main__':
+    region_df = pd.read_csv("scenarios/default_multi/regions.csv", sep=',')
+    tech_df = pd.read_csv("scenarios/default_multi/tech_data.csv", sep=',', index_col=0)
+    cfg_dict = {'stepsize': 15,
+                'soc_min': 0.2,
+                'rng': 3,
+                'eta_cp': 1,
+                'start_date': [2022, 5, 6],
+                'end_date': [2022, 6, 2]
+                }
+    s1 = SimBEV(region_df, {}, tech_df, cfg_dict)
+    s1.add_regions_from_dataframe()
+    print(s1.regions[0].cars[0].consumption)
+
+
