b77c73209a10bec0944bdeeea9c6e4b2b3529ecf
mosc5 <moritz.schiel@rl-institut.de>
2022-06-29 17:17:23 +0200

    update tests #41

--- tests/test_car.py
+++ tests/test_car.py
@@ -2,6 +2,6 @@ from simbev.car import Car, CarType
 
 
 def test_basic_car():
-    car_type = CarType("bev_mini", 30, {"slow": 11, "fast": 50}, {}, 0.14, False, "BEV")
+    car_type = CarType("bev_mini", 30, {"slow": 11, "fast": 50}, 0.2, 0.8, {}, {}, 0.14, False, "BEV")
     car = Car(car_type, 0, True, True, 11, 22, None)
     assert car._get_usecase(50) == "home"
