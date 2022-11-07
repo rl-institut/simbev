d8e5bf90968321c5291bd5d99907754609d79e26
Moritz.Schiel <moritz.schiel@rl-institut.de>
2022-11-07 13:10:22 +0100

    add timers

--- simbev/__main__.py
+++ simbev/__main__.py
@@ -2,13 +2,16 @@ import argparse
 from simbev.simbev_class import SimBEV
 import simbev.helpers.helpers as helpers
 import pathlib
+import datetime
 
 
 def main():
+    print(datetime.datetime.now())
     parser = argparse.ArgumentParser(description="SimBEV modelling tool for generating timeseries of electric "
                                                  "vehicles.")
     parser.add_argument("scenario", default="default", nargs='?',
                         help="Set the scenario which is located in ./scenarios .")
+    parser.add_argument("--timing", default=False, action='store_true')
     p_args = parser.parse_args()
 
     scenario_path = pathlib.Path("simbev", "scenarios", p_args.scenario)
