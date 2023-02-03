e6e744c8cb8ec116b7ba651067fef0a9c769242c
mosc5 <moritz.schiel@rl-institut.de>
2023-02-03 15:53:19 +0100

    fix pytest #87

--- tests/test_simbev.py
+++ tests/test_simbev.py
@@ -3,6 +3,6 @@ import pathlib
 
 
 def test_simbev_from_config():
-    scenario_path = pathlib.Path("simbev", "scenarios", "default")
+    scenario_path = pathlib.Path("scenarios", "default", "configs", "default.cfg")
     simbev_obj, cfg = SimBEV.from_config(scenario_path)
     assert simbev_obj.name == scenario_path.stem
