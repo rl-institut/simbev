83f033b0e6697de54706047fd9c517d961745090
JakobWeg <jakob.wegner@rl-institut.de>
2023-02-08 13:42:58 +0100

    update test for Class Car and CarType

--- tests/test_car.py
+++ tests/test_car.py
@@ -2,6 +2,9 @@ from simbev.car import Car, CarType
 
 
 def test_basic_car():
-    car_type = CarType("bev_mini", 30, {"slow": 11, "fast": 50}, 0.2, 0.8, {}, {}, 0.14, False, "BEV")
+    hpc_dict = {'soc_end_min': 0.8, 'soc_end_max': 0.95, 'soc_start_threshold': 0.6, 'park_time_max': 90.0,
+                'distance_min': 0.6, 'distance_max': 1.0, 'hpc_pref_A': 0.25, 'hpc_pref_B': 0.5, 'hpc_pref_C': 0.5,
+                'hpc_pref_D': 0.75}
+    car_type = CarType("bev_mini", 30, {"slow": 11, "fast": 50}, 0.2, 0.8, {}, {}, 0.14, False, hpc_dict, "BEV")
     car = Car(car_type, 0, True, True, 11, 22, None)
     assert car._get_usecase(50) == "home"
