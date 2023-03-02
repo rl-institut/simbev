a25a671bc8d8d29c34075b7581504d28c02dd2c7
JakobWeg <jakob.wegner@rl-institut.de>
2023-03-02 13:14:31 +0100

    update test for class Car and add test for class UserGroup

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,4 +1,4 @@
-from simbev.car import Car, CarType
+from simbev.car import Car, CarType, UserGroup
 
 
 def test_basic_car():
@@ -6,5 +6,6 @@ def test_basic_car():
                 'distance_min': 0.6, 'distance_max': 1.0, 'hpc_pref_A': 0.25, 'hpc_pref_B': 0.5, 'hpc_pref_C': 0.5,
                 'hpc_pref_D': 0.75}
     car_type = CarType("bev_mini", 30, {"slow": 11, "fast": 50}, 0.2, 0.8, {}, {}, 0.14, False, hpc_dict, "BEV")
-    car = Car(car_type, 0, True, True, 11, 22, None)
+    user_group = UserGroup(1, {})
+    car = Car(car_type, user_group, 0, True, True, 11, 22, None, True)
     assert car._get_usecase(50) == "home"
