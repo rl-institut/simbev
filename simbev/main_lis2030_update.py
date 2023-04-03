bc3fa5f2d231dfaa55769cc628e37612c8098e49
JakobWeg <jakob.wegner@rl-institut.de>
2023-04-03 10:15:23 +0200

    add alternative main for wrapper function

--- simbev/main_lis2030_update.py
+++ simbev/main_lis2030_update.py
@@ -3,60 +3,35 @@ from simbev.simbev_class import SimBEV
 import simbev.helpers.helpers as helpers
 import pathlib
 import datetime
-import pandas as pd
 import copy
 
 scenario_dict = {
 
     "multi_scenario_run": True,
-    "years": [2025, 2030, 2035],
-    "run_up_path": "lis_2030_update_input/run_up",
-    "scenarios": ["reference"], #, "low_availability", "high_availability", "digital_offers", "parking_management", "hpc"],
-    "scenario_paths": {"reference": "lis_2030_update_input/scenarios/1_reference",
-                       "low_availability": "lis_2030_update_input/scenarios/2_low_availability",
-                       "high_availability": "lis_2030_update_input/scenarios/3_high_availability",
-                       "digital_offers": "lis_2030_update_input/scenarios/4_digital_offers",
-                       "parking_management": "lis_2030_update_input/scenarios/5_parking_management",
-                       "hpc": "lis_2030_update_input/scenarios/6_hpc",
+    "years": [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2035],
+    "scenarios": ["reference", "low_availability", "high_availability", "digital_offers", "parking_management", "hpc"],
+    "scenario_paths": {"reference": "scenarios/default/configs/default.cfg",
+                       "low_availability": "scenarios/default/configs/default.cfg",
+                       "high_availability": "scenarios/default/configs/default.cfg",
+                       "digital_offers": "scenarios/default/configs/default.cfg",
+                       "parking_management": "scenarios/default/configs/default.cfg",
+                       "hpc": "scenarios/default/configs/default.cfg",
                        }
 }
 
 
-def wrapper(simbev_obj):
+def wrapper():
     print("---starting wrapper for lis_2030_update---")
     for year in scenario_dict["years"]:
-        print("-> Year:", year)
-
-        # read run-up
-        run_up_path = pathlib.Path(scenario_dict["run_up_path"], f"regions_{year}.csv")
-        run_up = pd.read_csv(
-            run_up_path,
-            sep=",",
-            index_col=0,
-        )
-
-        # read tech_data
-        tech_data_path = pathlib.Path(scenario_dict["run_up_path"], f"tech_data_{year}.csv")
-        tech_data = pd.read_csv(
-            tech_data_path,
-            sep=",",
-            index_col=0,
-        )
+        print(year)
 
         for scenario in scenario_dict["scenarios"]:
 
-            home_work_private_path = pathlib.Path(scenario_dict["scenario_paths"][scenario])
-            home_work_private = pd.read_csv(
-                pathlib.Path(
-                    home_work_private_path, f"home_work_private_{year}.csv"
-                )
-            )
-            home_work_private = home_work_private.set_index("region")
+            print(scenario)
+            config_path = pathlib.Path(scenario_dict["scenario_paths"][scenario])
 
-            print("-> Scenario:", scenario)
-            config_path = pathlib.Path(scenario_dict["scenario_paths"][scenario] + "/configs/default.cfg")
+            # run simbev here as module
 
-            # change config for run
             simbev_obj, cfg = SimBEV.from_config(config_path)
 
             path_parent = simbev_obj.save_directory.parent
@@ -65,12 +40,6 @@ def wrapper(simbev_obj):
                 path_parent, str(year), folder_name + "_scenario_" + str(scenario)
             )
             print(simbev_obj.save_directory)
-
-            # replace timedependent data (run_up, tech_data, home_work_private) in simbev_obj
-            simbev_obj.region_data = run_up
-            simbev_obj.tech_data = tech_data
-            simbev_obj.home_parking = home_work_private.loc["home", :]
-            simbev_obj.work_parking = home_work_private.loc["work", :]
 
             # run simulation with optional timing
             helpers.timeitlog(simbev_obj.output_options["timing"], simbev_obj.save_directory)(
@@ -88,7 +57,7 @@ def main():
     )
     parser.add_argument(
         "config_path",
-        default="lis_2030_update_input/scenarios/scenario_base/configs/default.cfg",
+        default="scenarios/default/configs/default.cfg",
         nargs="?",
         help="Set the config path.",
     )
@@ -100,7 +69,7 @@ def main():
 
     if scenario_dict["multi_scenario_run"]:
         # run analysis for specific parameter
-        wrapper(simbev_obj)
+        wrapper()
     else:
         # run simulation with optional timing
         helpers.timeitlog(simbev_obj.output_options["timing"], simbev_obj.save_directory)(
