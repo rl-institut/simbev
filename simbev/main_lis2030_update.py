b54cf4aa99ae124bade4041de71b4db75655e416
JakobWeg <jakob.wegner@rl-institut.de>
2023-04-04 13:38:54 +0200

    update pathes

--- simbev/main_lis2030_update.py
+++ simbev/main_lis2030_update.py
@@ -3,13 +3,15 @@ from simbev.simbev_class import SimBEV
 import simbev.helpers.helpers as helpers
 import pathlib
 import datetime
+import pandas as pd
 import copy
 
 scenario_dict = {
 
     "multi_scenario_run": True,
-    "years": [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2035],
-    "scenarios": ["reference", "low_availability", "high_availability", "digital_offers", "parking_management", "hpc"],
+    "years": [2025, 2030, 2035],
+    "run_up_path": "run_up",
+    "scenarios": ["reference"], #, "low_availability", "high_availability", "digital_offers", "parking_management", "hpc"],
     "scenario_paths": {"reference": "scenarios/default/configs/default.cfg",
                        "low_availability": "scenarios/default/configs/default.cfg",
                        "high_availability": "scenarios/default/configs/default.cfg",
@@ -23,15 +25,22 @@ scenario_dict = {
 def wrapper():
     print("---starting wrapper for lis_2030_update---")
     for year in scenario_dict["years"]:
-        print(year)
+        print("-> Year:", year)
+
+        # read run-up
+        run_up_path = pathlib.Path(scenario_dict["run_up_path"], f"regions_{year}.csv")
+        run_up = pd.read_csv(
+            run_up_path,
+            sep=",",
+            index_col=0,
+        )
 
         for scenario in scenario_dict["scenarios"]:
 
-            print(scenario)
+            print("-> Scenario:", scenario)
             config_path = pathlib.Path(scenario_dict["scenario_paths"][scenario])
 
-            # run simbev here as module
-
+            # change config for run
             simbev_obj, cfg = SimBEV.from_config(config_path)
 
             path_parent = simbev_obj.save_directory.parent
@@ -40,6 +49,9 @@ def wrapper():
                 path_parent, str(year), folder_name + "_scenario_" + str(scenario)
             )
             print(simbev_obj.save_directory)
+
+            # replace run_up in simbev_obj
+            simbev_obj.region_data = run_up
 
             # run simulation with optional timing
             helpers.timeitlog(simbev_obj.output_options["timing"], simbev_obj.save_directory)(
