c66d63d41242b398a0a930d4c808a1e975971969
JakobWeg <jakob.wegner@rl-institut.de>
2023-04-17 16:34:40 +0200

    update get_charging_use_case() function

--- simbev/car.py
+++ simbev/car.py
@@ -899,6 +899,8 @@ class Car:
         """
         if self.status == "driving":
             return ""
+        elif charging_use_case:
+            return charging_use_case
         elif self.work_parking and self.status == "work":
             return "work"
         elif self.home_parking and self.status == "home" and self.home_detached:
@@ -906,11 +908,11 @@ class Car:
         elif self.home_parking and self.status == "home" and not self.home_detached:
             return "home_apartment"
         # TODO: decide on status an requirement for hpc
-        elif (self.status == "hpc" and extra_urban) or charging_use_case == "highway_fast":
+        elif self.status == "hpc" and extra_urban:
             return "highway_fast"
-        elif power >= 150 or self.status == "hpc" or charging_use_case == "urban_fast":
+        elif power >= 150 or self.status == "hpc":
             return "urban_fast"
-        elif self.status == "shopping" or charging_use_case == "retail":
+        elif self.status == "shopping":
             return "retail"
         else:
             return "street"
