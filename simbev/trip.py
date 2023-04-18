f490214b48b5a222c33fd070940299788ea302b4
JakobWeg <jakob.wegner@rl-institut.de>
2023-04-18 09:23:58 +0200

    soften the midnight cutoff

--- simbev/trip.py
+++ simbev/trip.py
@@ -257,6 +257,7 @@ class Trip:
                     else:
                         max_parking_time = self.park_time
 
+
                     self.car.charge(
                         self,
                         station_capacity,
