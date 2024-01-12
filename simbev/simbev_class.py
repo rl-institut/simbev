2e01981a6effd54a22e3122b3d7ea2b38f07157f
mosc5 <moritz.schiel@rl-institut.de>
2024-01-12 11:01:17 +0100

    black

--- simbev/simbev_class.py
+++ simbev/simbev_class.py
@@ -1118,9 +1118,7 @@ class SimBEV:
             "threshold_street_night_limit": cfg.getfloat(
                 "basic", "threshold_street_night_limitation", fallback=21
             ),
-            "maximum_park_time": cfg.getint(
-                "basic", "maximum_park_time", fallback=10
-            ),
+            "maximum_park_time": cfg.getint("basic", "maximum_park_time", fallback=10),
             "maximum_park_time_flag": cfg.getboolean(
                 "basic", "maximum_park_time_flag", fallback=False
             ),
